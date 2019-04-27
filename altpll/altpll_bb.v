2c2da6ac191724d960167156e53d756d5b54aef0
team06_dclab <team06@dclab.com>
2019-04-28 04:41:16 +0800

    add qsys

---
+++ altpll/altpll_bb.v
@@ -0,0 +1,12 @@
+
+module altpll (
+	clk_clk,
+	reset_reset_n,
+	altpll_0_c0_clk,
+	altpll_0_c1_clk);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output		altpll_0_c0_clk;
+	output		altpll_0_c1_clk;
+endmodule
---
+++ altpll/altpll_inst.vhd
@@ -0,0 +1,17 @@
+	component altpll is
+		port (
+			clk_clk         : in  std_logic := 'X'; -- clk
+			reset_reset_n   : in  std_logic := 'X'; -- reset_n
+			altpll_0_c0_clk : out std_logic;        -- clk
+			altpll_0_c1_clk : out std_logic         -- clk
+		);
+	end component altpll;
+
+	u0 : component altpll
+		port map (
+			clk_clk         => CONNECTED_TO_clk_clk,         --         clk.clk
+			reset_reset_n   => CONNECTED_TO_reset_reset_n,   --       reset.reset_n
+			altpll_0_c0_clk => CONNECTED_TO_altpll_0_c0_clk, -- altpll_0_c0.clk
+			altpll_0_c1_clk => CONNECTED_TO_altpll_0_c1_clk  -- altpll_0_c1.clk
+		);
+
