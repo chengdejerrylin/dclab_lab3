9b1034e999fd1f314fbef734fabb0dd9c29418a3
whoami90506 <whoami90506@github.com>
2019-04-28 16:44:58 +0800

    add SRAM

--- src/I2S.sv
+++ src/I2S.sv
@@ -1,3 +1,29 @@
+module ADC (
+	input clk,    // Clock
+	input rst_n,  // Asynchronous reset active low
+
+	//WN8731
+	output reg AUD_ADCDAT,
+	input AUD_ADCLRCK,
+
+	//I2S
+	input start,
+	output reg [15:0] record_data,
+	output reg record_valid
+);
+	//contorl
+	localparam INIT = 'd0;
+	reg [:0] state n_state;
+
+	always_ff @(posedge clk or negedge rst_n) begin
+		if(~rst_n) begin
+			state <= INIT;
+		end else begin
+
+		end
+	end
+endmodule
+
 module I2S (
 	input clk,
 	input rst,
@@ -6,9 +32,9 @@ module I2S (
 	input AUD_ADCDAT,
 	inout AUD_ADCLRCK,
 	inout AUD_BCLK,
-	output reg AUD_DACDAT,
+	output AUD_DACDAT,
 	input AUD_DACLRCK,
-	output reg AUD_XCK,
+	output AUD_XCK,
 
 	//top
 	input [2:0] top_state,
@@ -21,7 +47,7 @@ module I2S (
 	output reg request_play_data,
 	input [15:0] play_data,
 	input play_valid
-
-	
 );
+
+assign AUD_XCK = clk;
 endmodule // I2S
