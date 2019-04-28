9c3d5f44d4b4cf2da21537b0106e40fb4a50364e
whoami90506 <whoami90506@github.com>
2019-04-28 18:47:19 +0800

    finish ADC

--- src/I2S.sv
+++ src/I2S.sv
@@ -11,27 +11,68 @@ module ADC (
 	output reg [15:0] record_data,
 	output reg record_valid
 );
-	//output
+	//input output
 	reg [15:0] n_record_data;
-	reg n_record_valid;
+	reg n_record_valid, _start;
 
 	//contorl
-	localparam INIT = 'd0;
-	localparam WAITL = 'd1;
-	localparam READ = 'd2;
-	localparam WAITH = 'd3;
+	localparam INIT = 2'd0;
+	localparam WAITL = 2'd1;
+	localparam READ = 2'd2;
+	localparam WAITH = 2'd3;
 
-	reg [:0] state, n_state;
+	reg [1:0] state, n_state;
 	reg [3:0] counter, n_counter;
 
-	always_ff @(posedge clk or negedge rst_n) begin
+	always_comb begin
+		case (state)
+			INIT : begin
+				if(_start) n_state = AUD_ADCLRCK ? WAITL : WAITH;
+				else n_state = INIT;
+
+				n_counter = 4'd0;
+				n_record_data = record_data;
+				n_record_valid = 1'd0;
+			end
+
+			WAITL : begin
+				n_state = ~AUD_ADCLRCK ? READ : WAITL;
+				n_counter = 4'd0;
+				n_record_data = record_data;
+				n_record_valid = 1'd0;
+			end
+
+			READ : begin
+				n_state = (counter == 4'd15) ? WAITH : READ;
+				n_counter = counter + 4'd1;
+				n_record_data = {record_data[14:0], AUD_ADCDAT};
+				n_record_valid = (counter == 4'd15);
+			end
+
+			WAITH : begin
+				n_state = AUD_ADCLRCK ? WAITL : WAITH;
+				n_counter = 4'd0;
+				n_record_data = record_data;
+				n_record_valid = 1'd0;
+			end
+
+		endcase
+	
+	end
+
+	always_ff @(negedge clk or negedge rst_n) begin
 		if(~rst_n) begin
 			state <= INIT;
 			counter <= 4'd0;
 			record_data <= 16'd0;
 			record_valid <= 1'd0;
+			_start <= 1'd0;
 		end else begin
-
+			state <= n_state;
+			counter <= n_counter;
+			record_data <= n_record_data;
+			record_valid <= n_record_valid;
+			_start <= start;
 		end
 	end
 endmodule
